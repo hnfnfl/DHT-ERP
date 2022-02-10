77c5660632dcc6ec6220819907d033f921d478c0
Hanif Naufal <kakaknaufal@gmail.com>
2022-02-10 11:59:19 +0700

    fix: add produk parameter at inquiries detail

--- erp/src/main/java/com/jaylangkung/dht/dht/inquiries/InquiriesDetailEntity.kt
+++ erp/src/main/java/com/jaylangkung/dht/dht/inquiries/InquiriesDetailEntity.kt
@@ -3,6 +3,7 @@ package com.jaylangkung.dht.dht.inquiries
 class InquiriesDetailEntity(
     var idproduk: String = "",
     var kode: String = "",
+    var produk: String = "",
     var harga: String = "",
     var jumlah: String = "",
     var sub_total: String = "",
