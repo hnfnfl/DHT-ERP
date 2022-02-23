616065ed33ad35bf60b85e0e6da139aa83d71162
Hanif Naufal <46749568+hnfnfl@users.noreply.github.com>
2022-02-23 09:15:41 +0700

    fix: remove onclick listener department adapter

--- erp/src/main/java/com/jaylangkung/dht/dht/department/DepartmentAdapter.kt
+++ erp/src/main/java/com/jaylangkung/dht/dht/department/DepartmentAdapter.kt
@@ -9,21 +9,12 @@ import com.jaylangkung.dht.databinding.ItemLevelBinding
 class DepartmentAdapter : RecyclerView.Adapter<DepartmentAdapter.ItemHolder>() {
 
     private var list = ArrayList<DepartmentEntity>()
-    private lateinit var onItemClickCallback: OnItemClickCallback
 
     fun setItem(item: List<DepartmentEntity>?) {
         if (item == null) return
         list.clear()
         list.addAll(item)
         notifyItemRangeChanged(0, item.size)
-    }
-
-    interface OnItemClickCallback {
-        fun onItemClicked(data: ArrayList<DepartmentEntity>, position: Int)
-    }
-
-    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
-        this.onItemClickCallback = onItemClickCallback
     }
 
     class ItemHolder(private val binding: ItemLevelBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -43,10 +34,6 @@ class DepartmentAdapter : RecyclerView.Adapter<DepartmentAdapter.ItemHolder>() {
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
         val item = list[position]
         holder.bind(item)
-
-        holder.itemView.setOnClickListener {
-            onItemClickCallback.onItemClicked(list, position)
-        }
     }
 
     override fun getItemCount(): Int = list.size
